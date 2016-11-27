4e0d7be40b263b00d2adace41cded5b18168cb4c
dunningm14 <dunningm14@georgefox.edu>
2016-11-26 23:15:29 -0600

    Add files via upload

---
+++ Drill 1/Overload_vs_Override.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_vs_Override
+//Overloading
+    public class test
+    {
+        public void getStuff(int id)
+        { }
+        public void getStuff(string name)
+        { }
+    }
+
+//Overriding
+    public class test
+    {
+        public virtual void getStuff(int id)
+        {
+            //Get stuff default location
+        }
+    }
+
+    public class test2 : test
+    {
+        public override void getStuff(int id)
+        {
+            //base.getStuff(id);
+            //or - Get stuff new location
+        }
+    }
+}
+
