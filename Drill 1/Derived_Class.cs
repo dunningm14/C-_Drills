097b3afb143f1e2343b8eb12e1231cdc6f0fe48f
dunningm14 <dunningm14@georgefox.edu>
2016-11-26 23:27:44 -0600

    Add files via upload

---
+++ Drill 1/Derived_Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derived_Class
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Constructs the Parent Class");
+        }
+
+        public void print()
+        {
+            Console.WriteLine("This is the Parent Class");
+        }
+    }
+
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("This is the Child Class");
+        }
+
+        public static void Main()
+        {
+            ChildClass child = new ChildClass();
+
+            child.print();
+        }
+    }
+}
