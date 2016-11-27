64e4065977d57c5071836e62ff10be8d69779c58
dunningm14 <dunningm14@georgefox.edu>
2016-11-26 23:51:05 -0600

    Add files via upload

---
+++ Drill 1/Sealed_Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Class
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            SealedClass sealedCls = new SealedClass();
+            int total = sealedCls.Add(4, 5);
+            Console.WriteLine("Total = " + total.ToString());
+        }
+    }
+    // Sealed class
+    sealed class SealedClass
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+}
