c70b142d43097839b1f3762e6cc7f57ee8e653d1
dunningm14 <dunningm14@georgefox.edu>
2016-11-27 23:32:19 -0600

    Add files via upload

---
+++ Drill 1/try_catch_finally.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class tryCatchFinally 
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 20;
+                int z = a + b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executed");
+            }
+        }
+    }
+}
