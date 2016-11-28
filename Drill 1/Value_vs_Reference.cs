f4e5a2af3e47f05c104bd27c8e892cba3a352b1f
dunningm14 <dunningm14@georgefox.edu>
2016-11-28 00:09:30 -0600

    Add files via upload

---
+++ Drill 1/Value_vs_Reference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valueTypesVsReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a value type
+            int x = 58;
+            // this is a reference type
+            int[] numbers = new int[11];
+        }
+    }
+}
