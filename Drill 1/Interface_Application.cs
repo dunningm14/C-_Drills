5fec7a607d5032e81a7537bcd00fd8f64a556a49
dunningm14 <dunningm14@georgefox.edu>
2016-11-28 00:42:02 -0600

    Update Interface_Application.cs

--- Drill 1/Interface_Application.cs
+++ Drill 1/Interface_Application.cs
@@ -3,55 +3,26 @@ using System.Linq;
 using System.Text;
 using System;
 
-namespace InterfaceApplication
+nnamespace InterfaceApplication
 {
-    public interface ITransactions
+    interface sayHello
     {
-        // interface members
-        void showTransaction();
-        double getAmount();
+        string printHello();
     }
-
-    public class Transaction : ITransactions
+    class Hello : IHello
     {
-        private string tCode;
-        private string date;
-        private double amount;
-        public Transaction()
+        public string printHello()
         {
-            tCode = " ";
-            date = " ";
-            amount = 0.0;
-        }
-
-        public Transaction(string c, string d, double a)
-        {
-            tCode = c;
-            date = d;
-            amount = a;
-        }
-
-        public double getAmount()
-        {
-            return amount;
-        }
-
-        public void showTransaction()
-        {
-            Console.WriteLine("Transaction: {0}", tCode);
-            Console.WriteLine("Date: {0}", date);
-            Console.WriteLine("Amount: {0}", getAmount());
+            Console.WriteLine("Hello!");
+            return null;
         }
     }
-    class Tester
+    class Program
     {
         static void Main(string[] args)
         {
-            Transaction t1 = new Transaction("001", "8/10/2012", 78900.00);
-            Transaction t2 = new Transaction("002", "9/10/2012", 451900.00);
-            t1.showTransaction();
-            t2.showTransaction();
-            Console.ReadKey();
+            Hello hi = new Hello();
+            hi.printHello();
         }
     }
 }
