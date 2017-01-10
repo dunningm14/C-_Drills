66fc923ec047a98f9c375a7928a32da467e76614
dunningm14 <dunningm14@georgefox.edu>
2017-01-09 20:19:02 -0600

    Update using_this.cs

--- Drill 1/using_this.cs
+++ Drill 1/using_this.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*************************************************************************************************************************
+Author: Madison Dunning
+Date: 11/28/16
+OS: Windows 10
+Purpose: To demonstrate the use of the keyword 'This'   
+         
+**************************************************************************************************************************/﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
