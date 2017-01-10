172ca80a19f5bfc3613b8554f14c6b95c58343f5
dunningm14 <dunningm14@georgefox.edu>
2017-01-09 20:24:54 -0600

    Update File_Transfer_Drill.cs

--- Drill 1/File_Transfer_Drill.cs
+++ Drill 1/File_Transfer_Drill.cs
@@ -1,4 +1,24 @@
-﻿using System;
+/*************************************************************************************************************************
+Author: Madison Dunning
+Date: 11/31/16
+OS: Windows 10
+Purpose: Your company's users create or edit a collection of text files
+         throughout the day. These text files represent data about customer
+         orders.
+         Once per day, any files that are new, or that were edited within the
+         previous 24 hours, must be sent to the home office. To facilitate this,
+         these new or updated files need to be copied to a specific 'destination'
+         folder on a computer, so that a special file transfer program can grab
+         them and transfer them to the home office.
+         The process of figuring out which files are new or recently edited, and
+         copying them to the 'destination' folder, is currently being done
+         manually. This is very expensive in terms of manpower.
+         You have been asked to create a script that will automate this task,
+         saving the company a lot of money over the long term.
+  
+**************************************************************************************************************************/﻿
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
